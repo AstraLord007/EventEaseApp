1ac34a5863adcc3fc12850d10eb04722fd193e00
AstraLord <mauridlenz@gmail.com>
2026-01-25 21:51:29 -0300

    routing fix

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EventEaseApp;
+using EventEaseApp.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Registrar EventService como Singleton
+builder.Services.AddSingleton<EventService>();
 
 await builder.Build().RunAsync();
